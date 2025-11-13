94540d27c1b6b57d3a2f798cb45ab2b8f0b34735
nnniyaz <nassyrovich@gmail.com>
2025-11-12 20:51:56 -0500

    test: check email config

--- server/pkg/email/email.go
+++ server/pkg/email/email.go
@@ -1,6 +1,8 @@
 package email
 
 import (
+	"fmt"
+
 	"github.com/nnniyaz/nop/server/config"
 	"gopkg.in/gomail.v2"
 )
@@ -14,6 +16,10 @@ type email struct {
 }
 
 func New(cfg *config.CfgEmail) (Email, error) {
+	fmt.Println("SMTP Host:", cfg.GetSmtpHost())
+	fmt.Println("SMTP Port:", cfg.GetSmtpPort())
+	fmt.Println("SMTP User:", cfg.GetSmtpUser())
+	fmt.Println("SMTP Pass:", cfg.GetSmtpPass())
 	d := gomail.NewDialer(cfg.GetSmtpHost(), int(cfg.GetSmtpPort()), cfg.GetSmtpUser(), cfg.GetSmtpPass())
 	if err := d.DialAndSend(); err != nil {
 		return nil, err
